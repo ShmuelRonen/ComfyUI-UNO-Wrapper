2bbd40e00453d521163c95c9a66f3974365faa94
Shmuel Ronen <80190186+ShmuelRonen@users.noreply.github.com>
2025-04-15 12:58:29 +0300

    init

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+ComfyUI-UNO-Wrapper
+A ComfyUI extension for integrating the UNO-FLUX model
+"""
+
+from .uno_nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
